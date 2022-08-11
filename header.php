cb265a3754eb31b1fa8e471a8e0a7424e12c4927
Vincent Dubroeucq <vincent@vincentdubroeucq.com>
2022-08-11 14:35:39 +0200

    Fixed header styling

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ $display_search = (bool) get_theme_mod( 'voyager_display_search', true );
             <?php get_template_part( 'template-parts/header', 'topbar' ); ?>
             <div class="navbar relative">
                 <div class="wrapper flex flex-wrap align-center maxw-xxl mx-auto py-2 px-4">
-                    <div class="site-branding lh-0 flex">
+                    <div class="site-branding lh-0 flex align-center">
                         <?php the_custom_logo(); $additional_class = display_header_text() ? '' : 'screen-reader-text'; ?>
                         <div class="site-title-block <?php echo $additional_class; ?>">
                             <a class="site-title naked bold uppercase grey-1 p-1" href="<?php echo esc_url( home_url() ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
@@ -42,7 +42,7 @@ $display_search = (bool) get_theme_mod( 'voyager_display_search', true );
                             <span class="screen-reader-text"><?php esc_html_e( 'Toggle Menu', 'voyager' ); ?></span>
                         </button>
                         <div id="main-menu-overlay" class="overlay main-menu-overlay text-center">
-                            <div class="overlay-wrapper p-5">
+                            <div class="overlay-wrapper p-4">
                                 <?php 
                                     wp_nav_menu( array(
                                         'theme_location' => 'menu-1',
