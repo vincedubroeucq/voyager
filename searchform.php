b6cdfb17123453959cad04575217001f73b85351
Vincent Dubroeucq <vincent@vincentdubroeucq.com>
2022-12-15 15:10:30 +0100

    Fixed search form styles

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
  * @package Voyager
  */
 ?>
-<form role="search" method="get" class="search-form flex flex-wrap flex-center" action="<?php echo esc_attr( home_url() );?>">
+<form role="search" method="get" class="search-form flex flex-wrap" action="<?php echo esc_attr( home_url() );?>">
     <label>
         <span class="screen-reader-text"><?php esc_html_e( 'Search:', 'voyager' )?></span>
         <input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Search the site', 'voyager' ); ?>" value="<?php echo get_search_query(); ?>" name="s">
