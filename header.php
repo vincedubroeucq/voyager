1c6975de6b8ecb9c16a8294b7a745ca39f6942a5
Vincent Dubroeucq <vincent@vincentdubroeucq.com>
2023-02-02 13:37:06 +0100

    Minor fixes

--- header.php
+++ header.php
@@ -26,7 +26,7 @@ $display_search = (bool) get_theme_mod( 'voyager_display_search', true );
     <header class="site-header relative">
         <div class="wrapper header-wrapper flex flex-column h-100">
             <?php voyager_header_image(); ?>
-            <?php get_template_part( 'template-parts/header', 'topbar' ); ?>
+            <?php get_template_part( 'template-parts/header-topbar' ); ?>
             <div class="navbar relative">
                 <div class="wrapper flex flex-wrap align-center maxw-xxl mx-auto px-5 py-2">
                     <div class="site-branding lh-0 flex align-center">
@@ -59,6 +59,6 @@ $display_search = (bool) get_theme_mod( 'voyager_display_search', true );
                     </nav>
                 </div>
             </div>
-            <?php get_template_part( 'template-parts/header', 'hero' ); ?>
+            <?php get_template_part( 'template-parts/header-hero' ); ?>
         </div>
     </header>
