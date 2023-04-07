17d4b3cde0c3532c908676f6610b123673e35915
Vincent Dubroeucq <vincent@vincentdubroeucq.com>
2023-04-07 14:53:15 +0200

    Removed useless Yoast functions

--- inc/yoast-seo-functions.php
+++ inc/yoast-seo-functions.php
@@ -3,17 +3,6 @@
  * Yoast SEO integration functions
  */
 
-add_filter( 'wpseo_breadcrumb_separator', 'voyager_breadcrumb_seperator' );
-/**
- * Adds a wrapper to breadcrumbs seperator.
- * 
- * @param   string  $seperator 
- * @return  string  $seperator 
- */
-function voyager_breadcrumb_seperator( $seperator ) {
-    return sprintf( '<span class="seperator mx-1">%s</span>', $seperator );
-}
-
 add_filter( 'wpseo_breadcrumb_single_link', 'voyager_breadcrumb_link', 10, 2 );
 /**
  * Adds classes to breadcrumb links
@@ -26,4 +15,3 @@ function voyager_breadcrumb_link( $link, $breadcrumb ) {
     $link = str_replace( '<a href=', '<a class="breadcrumb naked" href=', $link );
     return $link;
 }
-
